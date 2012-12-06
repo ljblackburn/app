a3f0fb3ee92c673728984e0023d37e4d31f493ec
git_name <git_email>
2012-12-06 18:02:40 -0500

    Committing

---
+++ source/app/web/application/catalogbrowsing/GenericFeature.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using app.web.application.catalogbrowsing.stubs;
+using app.web.core;
+using app.web.core.stubs;
+
+namespace app.web.application.catalogbrowsing
+{
+    public class GenericFeature<TRequestType, TItem> : ISupportAUserFeature
+    {
+        IFindInformationInTheStore store_catalog;
+        IDisplayInformation display_engine;
+        private FindData<TItem, TRequestType> data_finder;
+
+        public GenericFeature() : this(new StubStoreCatalog(), new StubDisplayEngine())
+        {
+        }
+
+        public GenericFeature(IFindInformationInTheStore information_in_the_store_catalog,
+                                                IDisplayInformation department_viewer)
+        {
+          this.store_catalog = information_in_the_store_catalog;
+          this.display_engine = department_viewer;
+        }
+
+        public FindData<TItem,TRequestType> datafinder
+        {
+            set { this.data_finder = value; }
+        }
+
+        public void process(IContainRequestDetails request)
+        {
+            var mapped_request = request.map<TRequestType>();
+            IEnumerable<TItem> departments = data_finder(mapped_request);
+            display_engine.display(departments);
+        }
+    }
+
+    public delegate IEnumerable<TItem> FindData<TItem, TRequestType>(TRequestType request);
+}
