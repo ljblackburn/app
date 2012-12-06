99e4bdeaee0eeb9ee97d0816fe84a8d3c9105fc8
ljblackburn <lblackburn@deltadentalmi.com>
2012-12-06 13:58:10 -0500

    Pushing new changes

--- source/app/web/CommandRegistry.cs
+++ source/app/web/CommandRegistry.cs
@@ -1,10 +1,23 @@
-﻿namespace app.web
+﻿using System.Collections.Generic;
+
+namespace app.web
 {
   public class CommandRegistry : IFindCommands
   {
+    IEnumerable<IRunOneRequest> all_the_possible_commands; 
+    
+    public CommandRegistry(IEnumerable<IRunOneRequest> all_the_possible_commands)
+    {
+        this.all_the_possible_commands = all_the_possible_commands;
+    }
+
     public IRunOneRequest get_the_command_that_can_run(IContainRequestDetails the_request)
     {
-      throw new System.NotImplementedException();
+        foreach (IRunOneRequest command in all_the_possible_commands)
+        {
+            if (command.can_run(the_request)) return command;
+        }
+        return null;
     }
   }
 }
